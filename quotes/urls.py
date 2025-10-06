1ecec9a49f97b98d9a820ce1720ef91ac03ace18
Jose Horacio <jhoracio19@hotmail.com>
2025-10-06 15:42:40 -0600

    refactorizando funcion

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path("<int:day>", views.days_week_with_number),
-    path("<str:day>", views.days_week)
+    path("<str:day>", views.days_week, name="day-quo te")
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
+days_of_week = {
+    "monday" : "Pienso luego existo",
+    "tuesday" : "La vida es un sueño",
+    "wednesday" : "El conocimiento es poder",
+    "thursday" : "Se el cambio que quieras ver en el mundo",
+    "friday" : "Solo se que no se nada",
+    "saturday" : "Vive con si fuera el ultimo dia",
+    "sunday" : "Da un poquito mas, todos los dias",
+}
 
 def days_week_with_number(request, day):
-    return HttpResponse(day)
+    days = list(days_of_week.keys())
+    if day > len(days):
+        return HttpResponseNotFound("El día no existe.")
+    redirect_day= days[day-1]
+    return HttpResponseRedirect(f"/quotes/{redirect_day}")
     
 
 def days_week(request, day):
-    quote_text = None
-    if day == 'monday':
-        quote_text = "Pienso, luego existo"
-    elif day == 'tuesday':
-        quote_text = "La vida es un sueño"
-    else:
-        return HttpResponseNotFound("No hay frase para este día")
-    
-    return HttpResponse(quote_text )
+    try:
+        quote_text = days_of_week[day]
+        return HttpResponse(quote_text)
+    except:
+        return HttpResponseNotFound("Este día no existe.")
