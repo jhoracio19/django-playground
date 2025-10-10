d3077bea38874a1a4d809716677378c8beb24c1e
Jose Horacio <jhoracio19@hotmail.com>
2025-10-10 16:16:16 -0600

    agregando dos tablas desde models a la bd

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birt_date = models.DateField(null=True, blank=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_date = models.DateField(null=True, blank=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='books')
+    pages = models.IntegerField()
+    isbn = models.CharField(max_length=50) 
