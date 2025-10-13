23484ad1b11b96ef1319cfe3bab6727b4918ddfc
Jose Horacio <jhoracio19@hotmail.com>
2025-10-12 22:14:13 -0600

    agregando modelo Genre

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -9,13 +9,21 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, related_name='books')
     pages = models.IntegerField()
-    isbn = models.CharField(max_length=50) 
+    isbn = models.CharField(max_length=50)
+    genres = models.ManyToManyField(Genre, related_name="books")
     
     def __str__(self):
         return self.title
