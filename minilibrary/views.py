0186f6094f9d4fb9ec55a977b27bd7cf60d3795e
Jose Horacio <jhoracio19@hotmail.com>
2025-10-16 23:58:29 -0600

    conservando filtros en paginador

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -10,20 +10,38 @@ from django.core.paginator import Paginator
 def index(request):
     try:
         books = Book.objects.all()
-        query = request.GET.get("query_search")
         
+        # Sirven para traer la info de la URL (metodo GET)
+        query = request.GET.get("query_search")
+        date_start = request.GET.get("start")
+        date_end = request.GET.get("end")
+        
+        # Los if son filtros
+        
+        # Filtro de libros por titulo o autor
         if query:
             books = books.filter(
                 Q(title__icontains=query) | Q(author__name__icontains=query)
             )
         
+        # Filtro 
+        if date_start and date_end:
+            books = books.filter(publication_date__range=[
+                date_start, date_end])
+        
         paginator = Paginator(books, 5)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
         
+        query_params = request.GET.copy()
+        if "page" in query_params:
+            query_params.pop("page")
+        query_strings = query_params.urlencode()
+        
         return render(request, "minilibrary/minilibrary.html",{
             "page_obj": page_obj,
-            "query": query
+            "query": query,
+            "query_string": query_strings
             
         })
     except Exception:
